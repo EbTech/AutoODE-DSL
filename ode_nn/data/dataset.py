31eb1f03ffebbf218987e5d75e1c2b6ed757e462
Tom McClintock <tmcclintock89@gmail.com>
2021-05-05 20:56:22 -0700

    Dataset works and the torch tensor matches. Still need a NaN mask

--- ode_nn/data/dataset.py
+++ ode_nn/data/dataset.py
@@ -7,9 +7,6 @@ Given N dates, M states (or geographic regions), and Q observables
 the dataframes will be of shape ``(N, Q * M)``, and the tensor
 will be of shape ``(N, Q, M)`` so that we can easily multiply on the
 M-by-M adjacency matrix.
-
-TODO items:
-
 """
 import os
 from datetime import date, datetime
@@ -19,16 +16,32 @@ from itertools import starmap
 from pathlib import Path
 from typing import List, Optional, Tuple, Union
 
+import numpy as np
 import pandas as pd
 import torch
 
 
 class Dataset(torch.utils.data.Dataset):
+    """
+    A torch-compatible dataset. This class reads in the data in the COVID-19
+    daily US report files and constructs the data into two forms: a pandas
+    dataframe accessible as the :attr:`df` attribute and a torch tensor
+    accessible as the :attr:`tensor` attribute.
+
+    Args:
+      datapath (Optional[Union[str, Path]]): default ``None``. Path to the
+        location of the daily reports. If ``None`` it defaults to the location
+        within the submodule (see the README)
+      dropped (Optional[List[str]]): default ``None``. List of the locations
+        to drop. This defaults to the two cruise ships, Guam, Virgin Islands,
+        Northern Mariana Islands, American Samoa, and an extraneous row in the
+        reports called "Recovered"
+    """
+
     def __init__(
         self,
         datapath: Optional[Union[str, Path]] = None,
         dropped: Optional[List[str]] = None,
-        date_rangs=Optional[Tuple[date, date]],
     ):
 
         self.datapath: Path = datapath or (
@@ -50,7 +63,20 @@ class Dataset(torch.utils.data.Dataset):
 
         dates, files = self.get_dates_and_files(self.datapath)
 
-        self.df = pd.concat(starmap(self.read_csv, zip(files, dates)), axis=0)
+        self.df: pd.DataFrame = pd.concat(
+            starmap(self.read_csv, zip(files, dates)), axis=0
+        )
+        self.tensor: torch.Tensor = torch.tensor(
+            self.df.to_numpy().reshape(len(self.df), 3, -1)
+        )
+
+    def __getitem__(self, index: int) -> torch.Tensor:
+        """Index the :attr:`tensor` attribute."""
+        return self.tensor[index]
+
+    def __len__(self) -> int:
+        """Length of the :attr:`tensor` attribute."""
+        return len(self.tensor)
 
     def get_dates_and_files(self, filepath: Path) -> Tuple[List[str], List[date]]:
         """
@@ -83,10 +109,19 @@ class Dataset(torch.utils.data.Dataset):
         return df
 
 
-ds = Dataset()
-df = ds.df
+if __name__ == "__main__":
+    ds = Dataset()
+    df = ds.df
+    tensor = ds.tensor
 
-print(df.head())
-# print(df.index)
-print(df.shape)
-# print(df.columns)
+    print(df.head())
+    print(df.shape)
+    print(tensor.shape)
+    print(df.iloc[0]["Confirmed"].values)
+    print(tensor[0, 0])
+    # Recovered and Deaths have NaNs so the comparison doesn't work, but
+    # by eye the values match
+    for i, name in enumerate(["Confirmed"]):  # , "Recovered", "Deaths"]):
+        for j in [0, 10, 20, -1]:  # just some numbers
+            assert (df.iloc[j][name].values == tensor[j, i].numpy()).all(), (name, j)
+            assert (df.iloc[j][name].values == ds[j][i].numpy()).all()
