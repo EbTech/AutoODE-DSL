d97c073bb1b1254fc760614820cf927924f516c0
Tom McClintock <tmcclintock89@gmail.com>
2021-05-29 15:54:03 -0700

    Tests of prob_S_E and one_step

--- tests/seiturd_test.py
+++ tests/seiturd_test.py
@@ -20,3 +20,26 @@ class SeiturdTest(TestCase):
             assert isinstance(pop, torch.Tensor)
             assert pop.shape == (1,)
         assert state.N == sum(state)
+
+    def test_prob_S_E(self):
+        n_regions = 50
+        I_t = torch.rand(n_regions)
+        model = Seiturd((42, 69, n_regions))
+        # TODO: replace this 'fake' adjacency matrix with something real
+        model.A = torch.rand((n_regions, n_regions))
+        pS = model.prob_S_E(I_t, t=0)
+        assert isinstance(pS, torch.Tensor)
+        assert pS.shape == I_t.shape
+
+    def test_one_step(self):
+        n_regions = 50
+        state = State(*torch.rand(7, n_regions))
+        ds_shape = (365, 420, n_regions)  # days, populations, states
+        model = Seiturd(ds_shape)
+        # TODO: replace this 'fake' adjacency matrix with something real
+        model.A = torch.rand((n_regions, n_regions))
+        t: int = 0
+        new_state = model.one_step(t, state)
+        for pop_name in list("SEITURD"):
+            # Check that there was a change
+            assert (getattr(new_state, pop_name) != getattr(state, pop_name)).all()
