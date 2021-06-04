a3f6b6f7b8564191534ae53aa89d7f7089958165
Tom McClintock <tmcclintock89@gmail.com>
2021-06-03 20:40:13 -0700

    Fixed a test

--- tests/lit_pandemic_test.py
+++ tests/lit_pandemic_test.py
@@ -2,13 +2,13 @@ from unittest import TestCase
 
 from torch.nn.functional import mse_loss
 
-from ode_nn import LitPandemic, Seiturd
+from ode_nn import LitPandemic, SeiturdModel
 
 
 class LitPandemitTest(TestCase):
     def test_smoke(self):
         """Smoke test of instantiation with a Seiturd model"""
-        model = Seiturd(365)
+        model = SeiturdModel(365)
         lp = LitPandemic(model=model, loss_fn=mse_loss)
         assert lp is not None
         assert list(lp.parameters()) == list(model.parameters())
