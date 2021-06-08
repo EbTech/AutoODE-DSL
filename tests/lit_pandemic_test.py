92bf09ff69b103e7076dc528cb44f7b81abeca4e
Tom McClintock <tmcclintock89@gmail.com>
2021-06-07 19:57:58 -0700

    Added a setupclass for the lit module that uses the data.

--- tests/lit_pandemic_test.py
+++ tests/lit_pandemic_test.py
@@ -3,12 +3,19 @@ from unittest import TestCase
 from torch.nn.functional import mse_loss
 
 from ode_nn import LitPandemic, SeiturdModel
+from ode_nn.data import C19Dataset
 
 
 class LitPandemitTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Create a dataset and save an adjacency matrix."""
+        cls.ds = C19Dataset()
+        cls.A = cls.ds.adjacency
+
     def test_smoke(self):
         """Smoke test of instantiation with a Seiturd model"""
-        model = SeiturdModel(365)
+        model = SeiturdModel(num_days=len(self.ds), adjacency_matrix=self.A)
         lp = LitPandemic(model=model, loss_fn=mse_loss)
         assert lp is not None
         assert list(lp.parameters()) == list(model.parameters())
