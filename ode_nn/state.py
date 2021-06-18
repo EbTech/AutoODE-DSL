947ca1309db4fa85acc62364dec09ab00c962fc9
Tom McClintock <tmcclintock89@gmail.com>
2021-06-18 08:08:17 -0700

    Requires_grad and device args

--- ode_nn/state.py
+++ ode_nn/state.py
@@ -1,7 +1,7 @@
 """
 The ``State`` object tracks populations of individuals in a compartment model.
 """
-from typing import Dict, Iterable, Union
+from typing import Dict, Iterable, Optional, Union
 
 import torch
 
@@ -25,6 +25,10 @@ class SmartState:
         A string of unique characters for the populations.
       n_days (int, optional): default is 100
       n_regions (int, optional): default is 50
+      requires_grad (bool, optional): default ``False``. flag to allow for
+        gradients
+      device (Optional[torch.device], optional): default ``None``. Send the
+        :attr:`tensor` to the device
     """
 
     def __init__(
@@ -32,6 +36,8 @@ class SmartState:
         populations: str = "SEIR",
         n_days: int = 100,
         n_regions: int = 50,
+        requires_grad: bool = False,
+        device: Optional[torch.device] = None,
     ) -> None:
         assert n_days > 0
         assert n_regions > 0
@@ -54,9 +60,28 @@ class SmartState:
         self.n_regions = n_regions
 
         # Tensor for the state
-        self.tensor = torch.zeros((n_days, self.n_populations, n_regions))
+        if device is None:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.tensor = torch.zeros(
+            (n_days, self.n_populations, n_regions),
+            device=device,
+            requires_grad=requires_grad,
+        )
 
     def __getitem__(self, key: Union[str, int, Iterable]) -> torch.Tensor:
+        """
+        Index the underlying tensor intelligently. If a string was passed in,
+        then return the ``(n_days, n_regions)`` tensor associated with that
+        population. If any other kind of index was passed int, index the
+        :attr:`tensor` attribute directly.
+
+        Args:
+          key (Union[str, int, Iterable]): either a population name or
+            numerical values to index the tensor
+
+        Returns:
+          the population(s) corresponding to the key
+        """
         # Handle pop name
         if isinstance(key, str):
             assert key in self._pop_set, f"invalid population {key}"
@@ -73,3 +98,10 @@ class SmartState:
           the total populations summed over all states
         """
         return self.tensor.sum(axis=1)
+
+    def __len__(self) -> int:
+        return len(self.tensor)
+
+    @property
+    def shape(self) -> torch.Size:
+        return self.tensor.shape
