69c3f2c64c6c03b9877bbe77aaef46dce48bf141
Tom McClintock <tmcclintock89@gmail.com>
2021-06-18 07:53:50 -0700

    Import

--- ode_nn/__init__.py
+++ ode_nn/__init__.py
@@ -2,3 +2,4 @@ from ode_nn.data.dataset import C19Dataset
 from ode_nn.lit_pandemic import LitPandemic
 from ode_nn.loss_fns import C19MSELoss
 from ode_nn.seiturd import History, SeiturdModel
+from ode_nn.state import SmartState
