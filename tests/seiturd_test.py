b279a4db689de09b859110ea20a945242c45fe4d
Tom McClintock <tmcclintock89@gmail.com>
2021-07-02 12:41:47 -0700

    Removed unused tests

--- tests/seiturd_test.py
+++ tests/seiturd_test.py
@@ -29,22 +29,8 @@ class SeiturdTest(TestCase):
         assert isinstance(pS, torch.Tensor)
         assert pS.shape == I_t.shape
 
-    def test_one_step(self):
-        n_regions = 50
-        A = torch.rand((n_regions, n_regions))
-        state = State(*torch.rand(7, n_regions))
-        model = SeiturdModel(365, A)
-        t: int = 0
-        new_state = model.run_one_step(state, t)
-        for pop_name in list("SEITURD"):
-            # Check that there was a change
-            assert (getattr(new_state, pop_name) != getattr(state, pop_name)).all()
+    def test_log_prob(self):
+        assert False
 
-    def test_run_forward(self):
-        n_regions = 50
-        A = torch.rand((n_regions, n_regions))
-        state = State(*torch.rand(7, n_regions))
-        model = SeiturdModel(365, A)
-        n_days = 10
-        hist = model.run_forward(state, 0, n_days)
-        assert hist.S.shape == (n_days, n_regions)
+    def test_flow_log_prob(Self):
+        assert False
