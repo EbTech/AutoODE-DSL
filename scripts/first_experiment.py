7f8d48311a179ee616a7beb8edc26358e7802a79
Danica J. Sutherland <djs@djsutherland.ml>
2021-07-23 14:44:56 -0700

    an optimization script that actually runs???????

--- scripts/first_experiment.py
+++ scripts/first_experiment.py
@@ -4,32 +4,23 @@ from ode_nn.data import C19Dataset
 from ode_nn.history import HistoryWithSoftmax
 from ode_nn.seiturd import History, SeiturdModel
 
+torch.set_default_dtype(torch.float64)
+
 ds = C19Dataset()
 history = HistoryWithSoftmax.from_dataset(ds, requires_grad=True)
 A = ds.adjacency
 
 model = SeiturdModel(history.num_days, A)
 
-log_prob = model.log_prob(history)
-total_log_prob = log_prob.sum()
+opt = torch.optim.SGD(list(model.parameters()) + list(history.parameters()), lr=1e-10)
 
-print(total_log_prob)
+for step in range(10):
+    print(f"starting step {step}")
+    opt.zero_grad()
 
-total_log_prob.backward()
-lr = 1e-5
+    log_prob = model.log_prob(history)
+    loss = -log_prob.sum()
 
-print("Logits SEIU before update:\n", history.logits_SEIU[-1, 0])
-print("Population of bama on day 1 before update:\n", history[-1, 0])
-# print("logic_decay_E before update: ", model.logit_decay_E)
-with torch.no_grad():
-    for name, param in model.named_parameters():
-        print(name)
-        param -= lr * param.grad
+    loss.backward()
 
-    for name, param in history.named_parameters():
-        print(name, param.grad.norm())
-        param -= lr * param.grad
-
-print("Logits SEIU after update:\n", history.logits_SEIU[-1, 0])
-print("Population of bama on day 1 after update:\n", history[-1, 0])
-# print("logic_decay_E after update: ", model.logit_decay_E)
+    opt.step()
