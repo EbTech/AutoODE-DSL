bb356f6d7040e521dae42a9cacf4625bc0b26478
Danica J. Sutherland <djs@djsutherland.ml>
2021-07-27 16:03:17 -0700

    fiddle with first_experiment

--- scripts/first_experiment.py
+++ scripts/first_experiment.py
@@ -11,17 +11,21 @@ ds = C19Dataset()
 history = HistoryWithSoftmax.from_dataset(ds, requires_grad=True)
 A = ds.adjacency
 
+
 model = SeiturdModel(history.num_days, A).to(device)
+print(
+    f"init:  E->: {model.decay_I:.3}   I->: {model.decay_E:.3}   T->: {model.decay_T:.3}"
+)
 
-opt = torch.optim.SGD(list(model.parameters()) + list(history.parameters()), lr=1e-10)
+opt = torch.optim.SGD(list(model.parameters()) + list(history.parameters()), lr=1e-12)
 
-for step in range(10):
-    print(f"starting step {step}")
+for step in range(100_000):
     opt.zero_grad()
-
     log_prob = model.log_prob(history)
     loss = -log_prob.sum()
-
     loss.backward()
-
     opt.step()
+    print(
+        f"step {step:5,}  loss: {loss.detach():15.3f}   "
+        f"E->: {model.decay_I:<5.3}   I->: {model.decay_E:<5.3}   T->: {model.decay_T:<5.3}"
+    )
