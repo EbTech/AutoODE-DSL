8d432d48b09a22d300bb8027c57654252409ca34
Danica J. Sutherland <djs@djsutherland.ml>
2021-09-07 15:58:46 -0700

    first_experiment: add the l1 regularizer

--- scripts/first_experiment.py
+++ scripts/first_experiment.py
@@ -17,14 +17,21 @@ print(
     f"init:  E->: {model.decay_I:.3}   I->: {model.decay_E:.3}   T->: {model.decay_T:.3}"
 )
 
-opt = torch.optim.SGD(list(model.parameters()) + list(history.parameters()), lr=1e-7)
+opt = torch.optim.Adam(list(model.parameters()) + list(history.parameters()), lr=1e-2)
 
 for step in range(100_000):
     opt.zero_grad()
-    loss = -model.log_prob(history)
+    regs = {}
+    for n in ["contagion_I", "detection_rate", "recovery_rate"]:
+        regs[n] = 100 * getattr(model, n).diff(dim=0).abs().sum()
+    loss = -model.log_prob(history) + sum(regs.values())
     loss.backward()
     opt.step()
     print(
         f"step {step:5,}  loss: {loss.detach():>15.3f}   "
-        f"E->: {model.decay_I:<5.3}   I->: {model.decay_E:<5.3}   T->: {model.decay_T:<5.3}"
+        f"E->: {model.decay_I:<5.3}   I->: {model.decay_E:<5.3}   T->: {model.decay_T:<5.3}   "
+        "regs: " + " ".join(f"{n}: {v:>10.0f}" for n, v in regs.items())
     )
+    if step % 2_000 == 0:
+        d = {"model": model, "history": history, "opt": opt}
+        torch.save(d, f"checkpt_{step}.pt")
